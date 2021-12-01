79f49708ee245f510c2a22e566a1632e41d0780f
Jon Whitter <me@jonwhitter.com>
2021-12-01 13:45:08 +0000

    day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content;
+
+            using(var reader = new StreamReader("input.txt"))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            var input = content.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => Convert.ToInt32(x));
+
+            Console.WriteLine($"Task 1: {input.Pairwise((prev, current) => current > prev).Where(x => x).Count()}");
+            Console.WriteLine($"Task 2: {input.Window(3).Where(x => x.Count == 3).Select(x => x.Sum()).Pairwise((prev, current) => current > prev).Where(x => x).Count()}");
+        }
+    }
+}
